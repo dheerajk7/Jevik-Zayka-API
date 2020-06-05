e35b29698a042ea36bb7fee3a68f125933364223
Dheeraj <Dheeraj77253@gmail.com>
2020-06-05 21:45:17 +0530

    setup express app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log('Error in running server');
+        return;
+    }
+    console.log('Server is running and up at port ',port);
+    return;
+});
