f83ace3b8a5d11e56746f735dae3f99402a9c23d
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-08-15 11:49:01 +0530

    updated kue configuration to use new redis

--- config/kue.js
+++ config/kue.js
@@ -1,6 +1,7 @@
-const kue = require("kue");
+const kue = require('kue');
+const env = require('./environment');
 
 //creating queue for delayed jobs
-const queue = kue.createQueue();
+const queue = kue.createQueue({ redis: env.redisURL });
 
 module.exports = queue;
